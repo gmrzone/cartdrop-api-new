e53e7f430f089b1692fe34173ab16d959ab0298a
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-08-15 01:23:56 +0530

    added migration script for coupon

--- src/db/migrations/files/00003_create-init-tables-core.sql
+++ src/db/migrations/files/00003_create-init-tables-core.sql
@@ -42,4 +42,75 @@ CREATE INDEX IF NOT EXISTS brands_slug_index02_like
     (slug COLLATE pg_catalog."default" varchar_pattern_ops ASC NULLS LAST)
     TABLESPACE pg_default;
 
+-- Coupon Table
+
+CREATE SEQUENCE IF NOT EXISTS coupon_codes_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE coupon_codes_id_seq
+    OWNER TO afzal;
+
+CREATE TABLE IF NOT EXISTS public.coupon_codes (
+    id bigint NOT NULL DEFAULT nextval('coupon_codes_id_seq'::regclass),
+    created timestamp with time zone NOT NULL,
+    updated timestamp with time zone NOT NULL,
+    uuid uuid NOT NULL,
+    code character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    discount integer NOT NULL,
+    valid_from  timestamp with time zone NOT NULL,
+    valid_to  timestamp with time zone NOT NULL,
+    active boolean NOT NULL,
+    reusable_type character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    summary character varying(200) COLLATE pg_catalog."default" NOT NULL,
+
+    CONSTRAINT coupon_code_pkey PRIMARY KEY(id),
+    CONSTRAINT coupon_code_uuid_unique UNIQUE(uuid),
+    CONSTRAINT coupon_code_discount_check CHECK (discount >=0 and discount <= 100)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.coupon_code
+    OWNER TO afzal;
+
+ALTER SEQUENCE coupon_codes_id_seq
+    OWNED BY coupon_codes.id;
+
+-- COUPON INDEXES
+
+CREATE INDEX IF NOT EXISTS coupon_codes_code_index01
+    ON public.coupon_codes USING btree
+    (code COLLATE pg_catalog."default" ASC NULLS LAST) 
+    TABLESPACE pg_default;
+
+CREATE INDEX IF NOT EXISTS coupon_code_code_index02_like
+    ON public.coupon_codes USING btree
+    (code COLLATE pg_catalog."default" varchar_pattern_ops ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+CREATE INDEX IF NOT EXISTS coupon_code_created_index03
+    ON public.coupon_codes USING btree
+    (created ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+CREATE INDEX IF NOT EXISTS coupon_code_valid_from_index03
+    ON public.coupon_codes USING btree
+    (valid_from ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+CREATE INDEX IF NOT EXISTS coupon_code_valid_to_index03
+    ON public.coupon_codes USING btree
+    (valid_to ASC NULLS LAST)
+    TABLESPACE pg_default;
+
+-- CATEGORY TABLES
+
+
+
+
+
 
