2c0abedefda222f371c20ebad8458c6d29076649
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-09-04 16:42:38 +0530

    enabled cors

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,5 +1,6 @@
 import { getVar } from '../../helpers';
 import { PoolConfig } from 'pg';
+import { CorsOptions } from 'cors';
 
 export const POOL_CONFIG: PoolConfig = {
   database: getVar('POSTGRES_DB_NAME'),
@@ -10,4 +11,15 @@ export const POOL_CONFIG: PoolConfig = {
   max: +getVar('DB_POOL_SIZE'),
   idleTimeoutMillis: +getVar('DB_POOL_CLIENT_IDLE_TIMEOUT'),
   connectionTimeoutMillis: +getVar('DB_POOL_CLIENT_CONNECTION_TIMEOUT'),
+};
+
+export const CORS_WHITE_LIST = ['http://127.0.0.1:3000/', 'http://localhost:3000/'];
+
+export const CORS_OPTIONS: CorsOptions = {
+  origin: (origin, callback) => {
+    if (origin && CORS_WHITE_LIST.indexOf(origin) === -1) {
+      return callback(new Error(`${origin} not allowed by CORS`));
+    }
+    callback(null, true);
+  },
 };
