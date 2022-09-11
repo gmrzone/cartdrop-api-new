bcd9a66bdeda0c17deaa29db845e39a8585ce1db
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-09-11 19:42:57 +0530

    refactoring

--- src/controllers/categories/index.ts
+++ src/controllers/categories/index.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
 import CategoryService from '../../services/categoryService';
 import { getBaseStaticUrl } from '../../helpers/index'
+import { generateErrorObject } from '../../helpers'
 
 export const getCategories = async (req: Request, res: Response) => {
-    const baseUrl = getBaseStaticUrl(req)
-    const { rows, rowCount } = await CategoryService.getCategoryWithImages(baseUrl)
-    res.setHeader('x-row-count',rowCount)
-    return res.status(200).json(rows)
+    try{
+        const baseUrl = getBaseStaticUrl(req)
+        const { rows, rowCount } = await CategoryService.getCategoryWithImages(baseUrl)
+        res.setHeader('x-row-count',rowCount)
+        return res.status(200).json(rows)
+    }
+    catch(err){
+        const errorObj = generateErrorObject(err, 500)
+        return res.status(500).json(errorObj)
+    }
 };
