7ea0a9e82c213ef9e5797fc4c57b2584c920d7fd
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-09-12 15:23:54 +0530

    added category controller test cases

--- src/controllers/categories/index.ts
+++ src/controllers/categories/index.ts
@@ -6,12 +6,14 @@ import { generateErrorObject } from '../../helpers'
 export const getCategories = async (req: Request, res: Response) => {
     try{
         const baseUrl = getBaseStaticUrl(req)
-        const { rows, rowCount } = await CategoryService.getCategoryWithImages(baseUrl)
+        const { rows, rowCount } = await CategoryService.getCategoriesWithImages(baseUrl)
         res.setHeader('x-row-count',rowCount)
-        return res.status(200).json(rows)
+        res.status(200)
+        return res.json(rows)
     }
     catch(err){
         const errorObj = generateErrorObject(err, 500)
-        return res.status(500).json(errorObj)
+        res.status(500)
+        return res.json(errorObj)
     }
 };
