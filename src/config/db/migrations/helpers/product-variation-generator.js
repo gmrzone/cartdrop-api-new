a316ebe3de4e02fffc639a150a9e5bbe3de68ed0
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-09-26 02:15:06 +0530

    added product images

---
+++ src/config/db/migrations/helpers/product-variation-generator.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const productVariationData = require('../../data/productVariation.json')
+
+
+productVariationData.forEach(row => {
+    if (row.table === "product_air_conditioner_variant"){
+
+    }
+    else if (row.table === "products_ram") {
+
+    }
+    else if (row.table === "products_storage") {
+        
+    }
+    else if (row.table === "product_mobile_variant") {
+        
+    }
+    else if (row.table === "product_laptop_variant") {
+        
+    }
+    else if (row.table === "product_variant") {
+        
+    }
+    else if (row.table === "product_variations") {
+        
+    }
+    else if (row.table === "product_variation_images") {
+
+    }
+})
