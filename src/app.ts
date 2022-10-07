401bd4d2f214546a44921d22465d4d906bb29dc6
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-10-08 02:28:12 +0530

    better response for rate limiter

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,8 @@ import categoryRoute from './routes/categories';
 import subcategoryRoutes from './routes/subcategories';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import { AugmentedRequest } from 'express-rate-limit';
+import { intervalToDuration } from 'date-fns';
 
 const swaggerDocument = YAML.load('./docs/cartdrop.yml');
 const app: Application = express();
@@ -15,12 +17,58 @@ const app: Application = express();
 // TODO : Remove express-rate-limit dependency and implement request rate limit middleware using redis
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: {
     status: 'error',
-    message: 'Too many requests, please try again later.',
+  },
+  handler: (request, response, next, options) => {
+    const {
+      rateLimit: { resetTime },
+    } = request as AugmentedRequest;
+    let formatedDate = '';
+    if (resetTime) {
+      const intervalObj = intervalToDuration({
+        start: resetTime,
+        end: new Date(),
+      });
+      if (intervalObj.years) {
+        formatedDate += `${intervalObj.years} Year${
+          intervalObj.years > 1 && 's'
+        }, `;
+      }
+      if (intervalObj.months) {
+        formatedDate += `${intervalObj.months} Month${
+          intervalObj.months > 1 && 's'
+        }, `;
+      }
+      if (intervalObj.days) {
+        formatedDate += `${intervalObj.days} Days${
+          intervalObj.days > 1 && 's'
+        }, `;
+      }
+      if (intervalObj.hours) {
+        formatedDate += `${intervalObj.hours} Hour${
+          intervalObj.hours > 1 && 's'
+        }, `;
+      }
+      if (intervalObj.minutes) {
+        formatedDate += `${intervalObj.minutes} Min${
+          intervalObj.minutes > 1 && 's'
+        }, `;
+      }
+      if (intervalObj.seconds) {
+        formatedDate += `${intervalObj.seconds} Sec${
+          intervalObj.seconds > 1 && 's'
+        }, `;
+      }
+    }
+    const updatedMessage = {
+      ...options.message,
+      message: `You have made too many requests, please try again after ${formatedDate}.`,
+    };
+    response.status(options.statusCode).send(updatedMessage);
   },
   skipFailedRequests: true,
 });
