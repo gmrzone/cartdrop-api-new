dcb3dd4062dc09753c1edaf75c2918772d485852
Afzal Saiyed <saiyedafzalgz@gmail.com>
2022-10-16 16:50:39 +0530

    added comments for paginationService

--- src/services/paginationService/index.ts
+++ src/services/paginationService/index.ts
@@ -67,10 +67,18 @@ export class PaginationService implements IPAGINATION_SERVICE {
     this._maxSize = maxSize;
     this._ordering = ordering;
   }
+  // Modify page size
   setPageSize = (size: number) => {
     this._pageSize = size;
   };
+  // this method is used to get all pagination params for SQL eg, condition, orderby, limit
+  // and it also return decoded cursor
   getPaginateParams = (cursor: string | undefined) => {
+    // if cursor is there then decode it else set to null
+    // if cursor is not there which means we are fetching first page so no need to add condition
+    // if cursor is there then we have to check if cursor is a reverse cursor
+    // if it is a reverse cursor then ordering will be DESC else ASC
+    // also if its a reverse cursor then condition will have < operator else > operator
     const cursorObj = cursor ? this.decodeCursor(cursor) : null;
     const reverse = cursorObj?.reverse;
     const position = cursorObj?.position;
@@ -85,7 +93,7 @@ export class PaginationService implements IPAGINATION_SERVICE {
   };
 
   parseOrdering = (isReverse: boolean) => {
-    // ordering for - functionality will be added later current will only add order by ASC
+    // TODO : ordering for - functionality will be added later current will only add order by ASC
     const isDescending = this._ordering.startsWith('-');
     const order = isReverse ? 'DESC' : 'ASC';
     const orderFieldName = isDescending
@@ -96,7 +104,7 @@ export class PaginationService implements IPAGINATION_SERVICE {
   };
 
   getCondition = (reverse?: boolean) => {
-    // for condition also will only support order by ASC for now later will add the remaning functionality
+    // TODO : for condition also will only support order by ASC for now later will add the remaning functionality
     const { order: __, orderFieldName } = this.parseOrdering(Boolean(reverse));
     const condition = `WHERE ${orderFieldName} ${reverse ? '<' : '>'} $1`;
     return condition;
@@ -110,6 +118,8 @@ export class PaginationService implements IPAGINATION_SERVICE {
     isReversed: boolean,
     _groupBy = false,
   ) => {
+    // This method will take base url, condition, orderBy, limit, isReversed and build a paginated SQL
+    // IF isReverse is true then we will use WITH SQL statement to reorder the reverse SQL to proper ordering
     if (condition) {
       baseSQL = baseSQL + ` ${condition}`;
     }
@@ -120,20 +130,27 @@ export class PaginationService implements IPAGINATION_SERVICE {
     }
     return SQL;
   };
+  // This method takes cursorId and reverse boolean and create a base64 cursor that
+  // we can later decode to get values for cursorId and reverse
   encodeCursor = (cursorId: string, reverse: boolean) => {
+    // create a object with reverse and position, if reverse is there then only we add it
     const queryParams = {
       ...(reverse && { r: 1 }),
       p: cursorId,
     };
+    // convert it to searchParam and then to string then create a buffer and convert to base64
     const queryString = new url.URLSearchParams(queryParams).toString();
     const cursorBuffer = Buffer.from(queryString, 'utf-8');
     const base64Cursor = cursorBuffer.toString('base64');
     return base64Cursor;
   };
 
+  // This method takes a cursor and returns the reverse and position value
   decodeCursor: (cursor: string) => { reverse?: number; position: string } = (
     cursor: string,
   ) => {
+    // create a buffer form base64 cursor then convert to string
+    // then convert to url search params ane get the calue for position and reverse
     const cursorBuffer = Buffer.from(cursor, 'base64');
     const cursorDecoded = cursorBuffer.toString('utf-8');
     const queryData = new url.URLSearchParams(cursorDecoded);
@@ -145,6 +162,8 @@ export class PaginationService implements IPAGINATION_SERVICE {
       position,
     };
   };
+  // this method should be called After SQL is executed. It takes items, rowCOunt, reqUrl, reverse and isCursorAvailable
+  // and return a paginated response with nextPageLink and previousPageLink
   getPaginatedResponse = <T extends { id: string }>(
     items: T[],
     rowCount: number,
@@ -152,6 +171,9 @@ export class PaginationService implements IPAGINATION_SERVICE {
     reverse: boolean,
     isCursorAvailable: boolean,
   ): { nextPage: string | null; previousPage: string | null; items: T[] } => {
+    // If rowCount is 1+ then the pageSize then we know that nextPage is available
+    // and if we are in reverse mode then we know that the previous page is available'
+    // we remove the extra item and pass it to getNextPage and getPreviousPage to get the link
     let updatedItems;
     if (rowCount === this._pageSize + 1) {
       updatedItems = reverse ? items.slice(1) : items.slice(0, -1);
@@ -173,9 +195,7 @@ export class PaginationService implements IPAGINATION_SERVICE {
       items: updatedItems,
     };
   };
-
-  // TODO: remove newCursorPosition and directly pass the list of data
-  // Also create a generic for type
+  // This method is used to get the previousPageLink if available else null
   getPreviousLink = <T extends { id: string }>(
     items: T[],
     rowCount: number,
@@ -183,6 +203,13 @@ export class PaginationService implements IPAGINATION_SERVICE {
     reverse: boolean,
     isCursorAvailable: boolean,
   ) => {
+    // if cursor is not available that means we are on first page and no previous page is available
+    // if we are not in reverse mode then we know there is a previous page
+    // if we are in reverse mode and rowCount return from SQL is 1+ page size then we have a previous page
+    // if any of the condition is true then we have to send a cursor for previous page else send null
+    // if its true then for previous page we will use first item id as cursorPosition
+    // and for the nextPageLink we will use lastItem id. in SQL we will use < or > to get the previous page or next page
+    // we encode the cursor and add it to request url
     if (
       isCursorAvailable &&
       (!reverse || (reverse && rowCount === this._pageSize + 1))
@@ -198,13 +225,17 @@ export class PaginationService implements IPAGINATION_SERVICE {
 
     return null;
   };
-
+  // This method is used to get the nextPageLink if available else null
   getNextLink = <T extends { id: string }>(
     items: T[],
     rowCount: number,
     baseUrl: string,
     reverse: boolean,
   ) => {
+    // if we get pageSize + 1 data from SQL that means we have next page
+    // or if we are in reverse then it means there is a next page
+    // for nextPAge Cursor position we use last item id and encode it to base64 and
+    // add it to the request url
     if (rowCount === this._pageSize + 1 || reverse) {
       const newCursorPosition = items[items.length - 1].id;
       const encodedCursor = this.encodeCursor(newCursorPosition, false);
